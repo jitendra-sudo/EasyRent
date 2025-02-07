742039d3179c9ad6cb9b18f7c5812e1857c22d71
Jitendra Saini <stupidboysbusiness@gmail.com>
2025-02-07 21:16:17 +0530

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
-import { BrowserRouter, Route , Routes } from 'react-router-dom'
-import Home from './components/home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
+import Statistics from './Components/Statistics';
+import Education from './Components/Education';
+import Contact from './Components/Contact';
+import './App.css';
 
-function App() {
+export function App() {
   return (
-   <>
-<BrowserRouter>
-  <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="*" element={<Home />} />
-  </Routes>
-</BrowserRouter>
-   </>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/education" element={<Education />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
